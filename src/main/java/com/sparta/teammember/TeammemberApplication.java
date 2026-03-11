64ccc6781753626aec4d876856c83a4346fc5c20
t2025-m0040 <e0321e@nate.com>
2026-03-11 21:42:14 +0900

    fix: 소스 파일 인식 오류 해결 및 로컬 구동 환경 설정 업데이트

---
+++ src/main/java/com/sparta/teammember/TeammemberApplication.java
@@ -0,0 +1,12 @@
+package com.sparta.teammember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeammemberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeammemberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/teammember/TeammemberApplicationTests.java
@@ -0,0 +1,12 @@
+package com.sparta.teammember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TeammemberApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+
+}
